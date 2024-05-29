f0cc0297c96080469256dcfd89d8c0fbcd36bdbf
Jefterddf28 <00001124407054sp@al.educacao.sp.gov.br>
2024-05-29 08:27:08 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}function setup() {
+  createCanvas(400, 400);
+  background("black")
+}
+
+function draw() {
+  stroke("blue");
+  fill("white");
+  
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
